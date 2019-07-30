bb2250c0bff44ee75bf11df9b1bf97ec3460e682
Juan Carlos Campos <rapidclimate@gmail.com>
2019-07-30 14:55:49 -0500

    payaso de rodeo:ven ven ven animalito ven

--- src/main/java/org/spring/Application.java
+++ src/main/java/org/spring/Application.java
@@ -1,17 +1,29 @@
 package org.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.List;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
+public class Application extends SpringBootServletInitializer implements CommandLineRunner {
+@Autowired RepoAlumno repoAlumno;
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+       // List<Alumno > alumnos=repoAlumno.findAll();
+     //   for(Alumno a:alumnos){
+     //       a.setId(a.getCuenta());
+       //     repoAlumno.save(a);
+      //  }
+    }
 }
